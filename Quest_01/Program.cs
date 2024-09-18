5cacf968b263b54e49a2fb7f795d5e30086ab62f
Jays <111653199+JophilGulane@users.noreply.github.com>
2024-09-18 11:44:53 +0800

    Answered Practice Sets 03 Function Drills

---
+++ Quest_01/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Quest_01
+{
+    internal class Program
+    {
+        //Create a function that computes the area of a triangle using Heron’s formula:
+        //Area = √s(s - a)(s - b)(s - c)
+        //where s is half the perimeter, or(a + b + c)/2
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length of 'a': ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of 'b': ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("Enter the length of 'c': ");
+            double c = double.Parse(Console.ReadLine());
+
+            double Area = AreaOfTriangle(a, b, c);
+
+            Console.WriteLine($"The Area of the triangle is: {Area}");
+        }
+
+        static double AreaOfTriangle(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            return area;
+        }
+    }
+}
---
+++ Quest_02/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Quest_02
+{
+    internal class Program
+    {
+        //Create a function that determines whether a given year is a leap year or not.
+        //A leap year is a year that is evenly divisible by 4, 
+        //except for years that are both divisible by 100 and not divisible by 400.
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a Year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            if (isLeapYear(year))
+            {
+                Console.WriteLine("is a Leap Year!");
+            }
+            else
+            {
+                Console.WriteLine("is NOT a Leap Year!");
+            }
+        }
+
+        static bool isLeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
